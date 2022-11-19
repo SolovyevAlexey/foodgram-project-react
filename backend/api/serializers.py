3d60e582de0706603c25615472aae72581d7967b
SolovyevAlexey <20032013alex@gmail.com>
2022-11-19 03:40:10 +0300

    HW 14 reworked

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -203,3 +203,15 @@ class RecordRecipeSerializer(FullRecipeSerializer):
         instance.tags.set(queryset_tags)
         instance.ingredients.set(queryset_amount_ingredients)
         return instance
+
+    def validate_ingredients(self, value):
+        ingredients = value
+        ingredients_list = []
+        for item in ingredients:
+            ingredient = item["name"].id
+            if ingredient in ingredients_list:
+                raise serializers.ValidationError({
+                    'ingredients': 'Ингредиенты не могут повторяться!'
+                })
+            ingredients_list.append(ingredient)
+        return value
